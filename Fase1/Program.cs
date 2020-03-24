5ccd946afd9ca6af2765446d9ec3c45949e0f6d7
dayte <dayte.rodriguez@gmail.com>
2020-03-24 18:01:40 +0100

    Creates an array containing the characters of the name and shows it on the Console

---
+++ Fase1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the array containing the characters of the name
+            char[] nameArray = { 'd', 'a', 'y', 't', 'é' };
+
+            showArray(nameArray);
+        }
+
+        private static void showArray(char[] aArray)
+        {
+            Console.WriteLine("Characters of the name: ");
+            for (int i = 0; i < aArray.Length; i++)
+            {
+                Console.WriteLine(aArray[i]);
+            }
+        }
+    }
+}

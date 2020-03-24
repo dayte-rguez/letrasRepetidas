3eae857524ae579f68b184c6189daed653156a11
dayte <dayte.rodriguez@gmail.com>
2020-03-24 18:11:24 +0100

    Changes the array for a list

---
+++ Fase2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the array containing the characters of the name
+            char[] nameArray = { 'd', 'a', 'y', 't', 'é' };
+
+            //Create the list that contains the characters of the name
+            var nameList = arrayToList(nameArray);
+
+            showArray(nameArray);
+        }
+
+        private static List<char> arrayToList(char[] aArray)
+        {
+            var listArray = new List<char>();
+
+            for (int i = 0; i < aArray.Length; i++)
+            {
+                listArray.Add(aArray[i]);
+            }
+            return listArray;
+        }
+
+        private static void showArray(char[] aArray)
+        {
+            Console.WriteLine("Characters of the name: ");
+            for (int i = 0; i < aArray.Length; i++)
+            {
+                Console.WriteLine(aArray[i]);
+            }
+        }
+    }
+}
+

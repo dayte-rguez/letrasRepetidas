203eca92023d3757d2da0ad8c67e4cd06f89ab01
dayte <dayte.rodriguez@gmail.com>
2020-03-24 18:25:34 +0100

    Analyzes the letters of the name

--- Fase2/Program.cs
+++ Fase2/Program.cs
@@ -13,7 +13,44 @@ namespace Fase1
             //Create the list that contains the characters of the name
             var nameList = arrayToList(nameArray);
 
-            showArray(nameArray);
+            nameAnalysis(nameList);
+        }
+
+        private static void nameAnalysis(List<char> aList)
+        {
+            foreach (var item in aList)
+            {
+                switch (item)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'á':
+                    case 'é':
+                    case 'í':
+                    case 'ó':
+                    case 'ú':
+                        Console.WriteLine($"{item} is a vowel");
+                        break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        Console.WriteLine($"People's names do not contain numbers");
+                        break;
+                    default:
+                        Console.WriteLine($"{item} is a consonant");
+                        break;
+                }
+            }
         }
 
         private static List<char> arrayToList(char[] aArray)
@@ -25,15 +62,6 @@ namespace Fase1
                 listArray.Add(aArray[i]);
             }
             return listArray;
-        }
-
-        private static void showArray(char[] aArray)
-        {
-            Console.WriteLine("Characters of the name: ");
-            for (int i = 0; i < aArray.Length; i++)
-            {
-                Console.WriteLine(aArray[i]);
-            }
         }
     }
 }
